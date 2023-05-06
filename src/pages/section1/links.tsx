1354c82a2aa4140e413369481535de45984e4de6
kjuiop <copying_y@naver.com>
2023-05-06 18:09:59 +0900

    [#1] feat: next/useRouter 설명 추가

--- src/pages/section1/links.tsx
+++ src/pages/section1/links.tsx
@@ -8,6 +8,11 @@ import { useEffect } from 'react';
 // 이는 최초에는 SSR 방식으로 페이지를 빨리 가져오고, 다음 페이지로 접근할 때에는 CSR 방식으로 JS 를 가져온다.
 // 이는 Next.js 의 link 로 인해 동작된다. (서버 갔다오지 않고, JS 로 랜더링됨)
 // 링크가 보이지 않을 때에는, js 파일을 가져오지 않고 있다가, 해당 링크가 화면에 보일 때에 Lazy Loading 방식으로 js 를 가져온다.
+
+// useRouter 는 next/link 를 대체할 수 있다.
+// useRouter 는 next/link 처럼 자동으로 화면이 보일 때 미리 js, json 파일을 땡겨오지 않는다.
+// 따라서 useEffect 함수를 직접 구현하여 prefetch 를 통해 해당 파일들을 가져올 수 있게 한다.
+// useEffect 는 자동으로 가져오지 않기 때문에 next/link 를 사용하는 것을 권장
 export default function Links() {
     const router = useRouter();
     useEffect(() => {
