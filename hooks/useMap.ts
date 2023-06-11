413150ddc9a50927d527f1e1c35e543e19fb4c56
kjuiop <copying_y@naver.com>
2023-06-11 18:53:01 +0900

    [#5] map custom header 를 통한 현재 지도 위치 url 로 복사 기능

--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -1,5 +1,5 @@
 import { useCallback } from 'react';
-import { mutate } from 'swr';
+import useSWR, { mutate } from 'swr';
 import { Coordinates } from '../types/store';
 import type { NaverMap } from '../types/map';
 
@@ -9,12 +9,32 @@ export const INITIAL_ZOOM = 10;
 export const MAP_KEY = '/map';
 
 const useMap = () => {
+
+    const { data: map } = useSWR(MAP_KEY);
+
     const initializeMap = useCallback((map: NaverMap) => {
         mutate(MAP_KEY, map);
     }, []);
 
+    // map.morph 을 실행하면 map 의 initial_center 와 initial_zoom 을 좌표로 naver 지도를 변경한다.
+    const resetMapOptions = useCallback(() => {
+        /** https://navermaps.github.io/maps.js.ncp/docs/naver.maps.Map.html#morph__anchor */
+        map.morph(new naver.maps.LatLng(...INITIAL_CENTER), INITIAL_ZOOM);
+    }, [map]);
+
+    // 현재 지도의 중심 좌표와 줌 레벨을 return 한다.
+    const getMapOptions = useCallback(() => {
+        const mapCenter = map.getCenter();
+        const center: Coordinates = [mapCenter.lat(), mapCenter.lng()];
+        const zoom = map.getZoom();
+
+        return { center, zoom };
+    }, [map]);
+
     return {
         initializeMap,
+        resetMapOptions,
+        getMapOptions,
     };
 };
 export default useMap;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,16 @@
-import {Inter} from '@next/font/google'
-import { Fragment, useEffect } from 'react';
-import Header from '../components/common/Header'
-import styles from '../styles/header.module.scss';
-import Link from 'next/link'
-import {AiOutlineShareAlt} from "react-icons/ai";
-import {VscFeedback} from "react-icons/vsc";
+import {Fragment, useEffect} from 'react';
+import Header from '../components/home/Header';
 import MapSection from '../components/home/MapSection'
 import {NextPage} from "next";
-import { Store } from '../types/store';
+import {Store} from '../types/store';
 import useStores from '../hooks/useStores'
-
-const inter = Inter({ subsets: ['latin'] })
 
 interface Props {
     stores: Store[]
 }
 
 const Home: NextPage<Props> = ({ stores }) => {
-    console.log("stores: ", stores);
+    // console.log("stores: ", stores);
 
     const { initializeStores } = useStores();
 
@@ -27,21 +20,7 @@ const Home: NextPage<Props> = ({ stores }) => {
 
     return (
         <Fragment>
-            <Header rightElements={[
-                // eslint-disable-next-line react/jsx-key
-                <button onClick={() => {
-                    alert('복사!')
-                }}
-                        className={styles.box}
-                        style={{ marginRight: 8 }}
-                        key="button"
-                >
-                    <AiOutlineShareAlt size={20} />
-                </button>,
-                <Link href="/feedback" className={styles.box} key="link">
-                    <VscFeedback size={20} />
-                </Link>
-            ]}/>
+            <Header />
             <main style={{ width: '100%', height: '100%' }}>
                 <MapSection />
             </main>
