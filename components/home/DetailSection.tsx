acebd012bc2a5a79e599bd3eb40112578c0ac71a
kjuiop <copying_y@naver.com>
2023-06-11 22:11:23 +0900

    [#7] style: Detail Header 추상화

--- components/home/DetailSection.tsx
+++ components/home/DetailSection.tsx
@@ -3,6 +3,7 @@ import { IoIosArrowUp } from 'react-icons/io';
 import useSWR from "swr";
 import {CURRENT_STORE_KEY} from "../../hooks/useCurrentStore";
 import {useState} from "react";
+import DetailHeader from './DetailHeader';
 import DetailContent from '../home/DetailContent'
 
 const DetailSection = () => {
@@ -15,17 +16,11 @@ const DetailSection = () => {
                 currentStore ? styles.selected : ''
             }`}
         >
-            <div className={styles.header}>
-                <button
-                    className={`${styles.arrowButton} ${expanded ? styles.expanded : ''}`}
-                    onClick={() => setExpanded(!expanded)}
-                    disabled={!currentStore}
-                >
-                    <IoIosArrowUp size={20} color="#666666" />
-                </button>
-                {!currentStore && <p className={styles.title}>매장을 선택해주세요</p>}
-                {currentStore && <p className={styles.title}>{currentStore.name}</p>}
-            </div>
+            <DetailHeader
+                currentStore={currentStore}
+                expanded={expanded}
+                onClickArrow={() => setExpanded(!expanded)}
+            />
             <DetailContent currentStore={currentStore} expanded={expanded} />
         </div>
     );
