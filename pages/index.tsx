5d8a0373e064c6db7787e6772201c57afc4b97ab
kjuiop <copying_y@naver.com>
2023-06-17 18:25:55 +0900

    [#22] feat: stores api 와 가게정보 목록 조회 연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,6 @@ interface Props {
 }
 
 const Home: NextPage<Props> = ({ stores }) => {
-    // console.log("stores: ", stores);
-
-    useEffect(() => {
-        fetch('/api/stores').then((response) => response.json().then(console.log));
-    }, []);
-
     const { initializeStores } = useStores();
 
     useEffect(() => {
@@ -43,7 +37,7 @@ export default Home;
 
 export async function getStaticProps() {
     // todo: next api routes 로 불러오기
-    const stores = (await import('../public/store.json')).default;
+    const stores = await fetch('http://localhost:3000/api/stores').then((response) => response.json());
 
     // revalidate : 1시간, reloading 하는 주기
     return {
