c6c190353bbf6f2f19be0212a7f2269a95aaeddb
kjuiop <copying_y@naver.com>
2023-06-17 09:20:48 +0900

    [#9] feat: _document 에 title, meta 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <title>Catch Review</title>
+            <meta name="description" content="가게에 대한 리뷰를 작성하고 공유하는 소셜 커뮤니티입니다." />
+        </Head>
       <body>
         <Main />
         <NextScript />
