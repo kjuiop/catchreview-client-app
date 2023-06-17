ed74b6798cab9a3db39bc0126f879cdde42a15e0
kjuiop <copying_y@naver.com>
2023-06-17 09:35:42 +0900

    [#9] feat: html lang 를 국제 표준 및 언어의 후보 배열로 적용

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['inflearn-nextjs.vercel.app', 'search.pstatic.net']
-  }
+  },
+  i18n: {
+    /** https://nextjs.org/docs/advanced-features/i18n-routing#getting-started */
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
 }
 
 module.exports = nextConfig
